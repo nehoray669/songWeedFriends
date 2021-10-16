15b2ee45be602525953a78ad37fa099ae1bfb67c
nehoray669 <nehorayregimov@gmail.com>
2021-10-16 20:56:25 +0300

    updated mongo db
    created get user id function
    re factored code

--- Client.cs
+++ Client.cs
@@ -5,11 +5,23 @@ namespace mongoDb
 {
     public class Client
     {
+        public Client()
+        {
+        }
+
+        public Client(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+            this.isLobbyOpen = false;
+        }
+
         [BsonId]
         public Guid Id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string songName { get; set; }
         public string currTime { get; set; }
+        public bool isLobbyOpen { get; set; }
     }
 }
--- MongoCRUD.cs
+++ MongoCRUD.cs
@@ -15,20 +15,20 @@ namespace mongoDb
             db = client.GetDatabase(database);
         }
 
-        public void InsertRecord<T>(string table, T record)
+        public void InsertUser<T>(string table, T record)
         {
             var collection = db.GetCollection<T>(table);
             collection.InsertOne(record);
         }
 
-        public List<T> LoadRecords<T>(string table)
+        public List<T> LoadUsers<T>(string table)
         {
             var collection = db.GetCollection<T>(table);
 
             return collection.Find(new BsonDocument()).ToList(); 
         }
 
-        public T LoadRecordById<T>(string table, Guid id) 
+        public T LoadUserById<T>(string table, Guid id) 
         {
             var collection = db.GetCollection<T>(table);
 
@@ -39,8 +39,8 @@ namespace mongoDb
             return collection.Find(filter).First();
         }
 
-        //login function
-        public void UpsertRecored<T>(string table, Guid id, T record) 
+        //upsert is update or insert
+        public void UpsertUser<T>(string table, Guid id, T record) 
         {
             var collection = db.GetCollection<T>(table);
 
@@ -50,11 +50,18 @@ namespace mongoDb
                 new UpdateOptions { IsUpsert = true });
         }
 
-        public void DeleteRecord<T>(string table, Guid id)
+        public void DeleteUserById<T>(string table, Guid id)
         {
             var collection = db.GetCollection<T>(table);
             var filter = Builders<T>.Filter.Eq("Id", id);
             collection.DeleteOne(filter);
+        }
+
+        public T GetUserId<T>(string table, string username)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("username", username);
+            return collection.Find(filter).First();
         }
     }
 }
